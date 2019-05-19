cead5f561f4fd07a19bc860594811dde8289ba19
priyankakaruppiya <kavipriyamca2518@gmail.com>
2019-05-19 12:24:38 +0530

    Modified File

--- Welcome.java
+++ Welcome.java
@@ -4,8 +4,10 @@ public static void main(String[] args) {
 
         System.out.println("Welcome.....!!");
 
+System.out.println("Go to LoginPage.....!!");
 
-		}
+		System.out.println("Go to RegisterPage.....!!");
 
 
+}
 }
